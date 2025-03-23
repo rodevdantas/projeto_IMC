ce637002c4a9c5061171e31f58937af902c85e8a
YoSoyRod <rodrigocontatodantas@gmail.com>
2025-03-23 19:39:51 -0300

    first commit

---
+++ imc.js
@@ -0,0 +1,60 @@
+var botao = document.getElementById('btnExecutar');
+
+botao.addEventListener('click', calcularIMC);
+
+function calcularIMC() 
+{
+    var nomeUsuario = document.getElementById('inputNome').value.trim();
+    var alturaUsuario = parseFloat(document.getElementById('inputAltura').value);
+    var pesoUsuario = parseFloat (document.getElementById('inputPeso').value);
+    var resultado = document.getElementById('exibirResultado');
+    
+    if (nomeUsuario !== '' && alturaUsuario !== '' && pesoUsuario !== '')
+
+    {
+        var imc = (pesoUsuario / (alturaUsuario * alturaUsuario)).toFixed(1);
+        
+        var status = '';
+
+        if (imc < 18.5)
+        {
+            status = 'abaixo do peso';
+        }
+
+        else if (imc < 25)
+        {
+            status = 'com o peso adequado';
+        }
+
+        else if (imc < 30)
+        { 
+            status = 'com sobrepeso';
+        }
+
+        else if (imc < 35)
+        {
+            status = 'com obesidade leve';
+        }
+
+        else if (imc < 40)
+        { 
+            status = 'obesidade moderada'
+        }
+
+        else 
+        {
+            status = 'CARALHO, VAI SE CUIDAR PORRA' 
+        }
+
+        resultado.textContent = `${nomeUsuario}, seu IMC é ${imc} e você está ${status}`;
+
+    }
+
+    else 
+    {
+        resultado.textContent = 'Por favor, preencha todos os campos corretamente.';
+    }
+
+
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+ #Calculadora - IMC
+
+ Projeto de uma calculadora de IMC utilizando HTML, CSS e JavaScript feito do zero.
+
