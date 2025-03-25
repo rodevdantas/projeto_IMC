dd5113b9bd39fd6c9aa5020f87be4a5af3107b3b
YoSoyRod <rodrigocontatodantas@gmail.com>
2025-03-25 14:33:43 -0300

    change resultado.textContent

--- imc.js
+++ imc.js
@@ -18,32 +18,32 @@ function calcularIMC()
 
         if (imc < 18.5)
         {
-            status = 'abaixo do peso';
+            status = 'abaixo do peso.';
         }
 
         else if (imc < 25)
         {
-            status = 'com o peso adequado';
+            status = 'com o peso adequado.';
         }
 
         else if (imc < 30)
         { 
-            status = 'com sobrepeso';
+            status = 'com sobrepeso.';
         }
 
         else if (imc < 35)
         {
-            status = 'com obesidade leve';
+            status = 'com obesidade leve.';
         }
 
         else if (imc < 40)
         { 
-            status = 'obesidade moderada'
+            status = 'obesidade moderada.'
         }
 
         else 
         {
-            status = 'CARALHO, VAI SE CUIDAR PORRA' 
+            status = 'EXTREMAMENTE obeso. Cuide-se para não morrer.' 
         }
 
         resultado.textContent = `${nomeUsuario}, seu IMC é ${imc} e você está ${status}`;
